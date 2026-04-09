7e2122b6a4fa0316f59503fb7c9181549db66a85
kdy20716-droid <kdy20716@gmail.com>
2026-04-09 18:16:53 +0900

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,26 @@
 import "./Header.css";
 import accountIcon from "../assets/account-icon.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="site-header">
       <div className="header-inner">
-        <a href="#" className="brand">
+        <Link to="/" className="brand">
           PICKPICK
-        </a>
+        </Link>
         <nav className="site-nav" aria-label="주요 메뉴">
           <a href="#">+ CREATE</a>
           <a href="#">RANKING</a>
-          <a href="#">LOG IN</a>
-          <a href="#" className="account-link" aria-label="계정">
-            <img src={accountIcon} alt="" />
-          </a>
+          <Link to="/signin">LOG IN</Link>
+          <div className="dropdown">
+            <a href="#" className="account-link" aria-label="계정">
+              <img src={accountIcon} alt="마이페이지" />
+            </a>
+            <div className="dropdown-content">
+              <Link to="/result">투표 결과 모아보기</Link>
+            </div>
+          </div>
         </nav>
       </div>
       <div className="header-glow" aria-hidden="true" />
