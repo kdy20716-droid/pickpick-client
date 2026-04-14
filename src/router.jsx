ad5327922a76ce1eefa4c296aa7e5d8edfc32b75
kdy20716-droid <kdy20716@gmail.com>
2026-04-14 20:37:37 +0900

    .

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
-import RecipeAddPage from "./pages/RecipeAddPage";
 import Layout from "./components/Layout";
-import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
-import SignupPage from "./pages/SignupPage";
-import RecipeDetailPage from "./pages/RecipeDetailPage";
+import Login from "./pages/Login";
+import Signin from "./pages/Signin";
+import Findpass from "./pages/Findpass";
+import Ranking from "./pages/Ranking";
+import Result from "./pages/Result";
+import Create from "./pages/Create";
+import VotePage from "./pages/VotePage";
 import MyPage from "./pages/MyPage";
 
 const router = createBrowserRouter([
@@ -14,24 +17,36 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <MainPage /> },
       {
-        path: "/add",
-        element: <RecipeAddPage />,
-      },
-      {
         path: "/login",
-        element: <LoginPage />,
+        element: <Login />,
       },
       {
-        path: "/signup",
-        element: <SignupPage />,
+        path: "/signin",
+        element: <Signin />,
+      },
+      {
+        path: "/findpass",
+        element: <Findpass />,
+      },
+      {
+        path: "/ranking",
+        element: <Ranking />,
+      },
+      {
+        path: "/result",
+        element: <Result />,
+      },
+      {
+        path: "/create",
+        element: <Create />,
+      },
+      {
+        path: "/vote",
+        element: <VotePage />,
       },
       {
         path: "/mypage",
         element: <MyPage />,
-      },
-      {
-        path: "/recipes/:id",
-        element: <RecipeDetailPage />,
       },
     ],
   },
