de0a2d880f7fb590c6ce765abd67e3960d02bc7e
kdy20716-droid <kdy20716@gmail.com>
2026-04-15 22:11:35 +0900

    .

--- src/pages/Findpass.jsx
+++ src/pages/Findpass.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./findpass.css";
+import "./Findpass.css";
 
 export default function FindPassword() {
   const [email, setEmail] = useState("");
