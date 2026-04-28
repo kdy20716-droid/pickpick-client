507e373e6b61ea28238545f2ef4858795a1f25a8
hios05 <yunibus02@gmail.com>
2026-04-28 21:34:46 +0900

    마이페이지 투표 결과 모아보기, 바 수정

---
+++ src/components/menu.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+const Menu = () => {
+  return (
+    <nav className="menu-nav">
+      <Link to="/mypage/history">HISTORY</Link>
+      <Link to="/mypage/like">LIKE</Link>
+      <Link to="/mypage/mypoll">MY POLL</Link>
+      <Link to="/mypage/contact">CONTACT</Link>
+      <Link to="/mypage" className="active">
+        MY PAGE
+      </Link>
+    </nav>
+  );
+};
+
+export default Menu;
--- src/router.jsx
+++ src/router.jsx
@@ -9,6 +9,7 @@ import Result from "./pages/Result";
 import Create from "./pages/Create";
 import VotePage from "./pages/VotePage";
 import MyPage from "./pages/MyPage";
+import Profile from "./pages/Profile";
 
 const router = createBrowserRouter([
   {
@@ -46,8 +47,33 @@ const router = createBrowserRouter([
       },
       {
         path: "/mypage",
-        element: <MyPage />
-      }
+        element: <MyPage />,
+        children: [
+          { index: true, element: <Profile /> },
+          {
+            path: "history",
+            element: <Result />,
+          },
+          {
+            path: "like",
+            element: (
+              <div style={{ padding: "40px" }}>LIKE PAGE (COMING SOON)</div>
+            ),
+          },
+          {
+            path: "mypoll",
+            element: (
+              <div style={{ padding: "40px" }}>MY POLL PAGE (COMING SOON)</div>
+            ),
+          },
+          {
+            path: "contact",
+            element: (
+              <div style={{ padding: "40px" }}>CONTACT PAGE (COMING SOON)</div>
+            ),
+          },
+        ],
+      },
     ],
   },
 ]);
