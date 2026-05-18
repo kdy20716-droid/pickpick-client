a846b4b25650361941485c66c3084c7611a7557e
kdy20716-droid <kdy20716@gmail.com>
2026-05-18 20:47:41 +0900

    투표페이지에서 재생 테스트

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -83,7 +83,7 @@ const Create = () => {
   ];
 
   const getYouTubeId = (url) => {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|shorts\/)([^#\&\?]*).*/;
     const match = url.match(regExp);
     return (match && match[2].length === 11) ? match[2] : null;
   };
--- src/pages/VotePage.jsx
+++ src/pages/VotePage.jsx
@@ -310,12 +310,11 @@ function VoteCard({
                        <iframe
                         width="100%"
                         height="100%"
-                        src={`https://www.youtube.com/embed/${candidate.image}?autoplay=1&mute=1&controls=0&loop=1&playlist=${candidate.image}&rel=0`}
+                        src={`https://www.youtube.com/embed/${candidate.image}?autoplay=1&mute=1&controls=0&loop=1&playlist=${candidate.image}&rel=0&playsinline=1&enablejsapi=1`}
                         title={candidate.name}
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen
-                        style={{ pointerEvents: "none" }}
                       />
                     </div>
                   ) : candidate.type === "video" ? (
