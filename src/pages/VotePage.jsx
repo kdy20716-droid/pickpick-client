34c6fe49b38c791ef0b6a8aead6affc6d46f7000
kdy20716-droid <kdy20716@gmail.com>
2026-05-19 17:33:08 +0900

    유튜브 음량 수정중

--- src/pages/VotePage.jsx
+++ src/pages/VotePage.jsx
@@ -313,7 +313,7 @@ function YouTubePlayer({ videoId, title, isActive }) {
           ref={iframeRef}
           width="100%"
           height="100%"
-          src={`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=0&mute=1&controls=0&loop=1&playlist=${videoId}&rel=0&playsinline=1&enablejsapi=1`}
+          src={`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=0&mute=0&controls=1&loop=1&playlist=${videoId}&rel=0&playsinline=1&enablejsapi=1`}
           title={title}
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
