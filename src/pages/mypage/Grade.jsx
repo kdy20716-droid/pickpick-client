9f0e062f0103ca4e54611bcc89bf974ce436a8b1
kdy20716-droid <kdy20716@gmail.com>
2026-05-19 20:57:05 +0900

    관리자 권한, 티어색깔

--- src/pages/mypage/Grade.jsx
+++ src/pages/mypage/Grade.jsx
@@ -15,7 +15,7 @@ const Grade = () => {
             <div className="gradeIconWrapper profile-border-bronze">
               <div className="gradeIconPlaceholder"></div>
             </div>
-            <span className="gradeName">BRONZE</span>
+            <span className="gradeName grade-bronze">BRONZE</span>
             <div className="gradeRequirements">
               <span className="gradeRequirement">투표 참여</span>
               <span className="gradeRequirement">10회 이상</span>
@@ -25,7 +25,7 @@ const Grade = () => {
             <div className="gradeIconWrapper profile-border-silver">
               <div className="gradeIconPlaceholder"></div>
             </div>
-            <span className="gradeName">SILVER</span>
+            <span className="gradeName grade-silver">SILVER</span>
             <div className="gradeRequirements">
               <span className="gradeRequirement">참여 100 / 생성 10</span>
               <span className="gradeRequirement">이상 달성</span>
@@ -35,7 +35,7 @@ const Grade = () => {
             <div className="gradeIconWrapper profile-border-gold">
               <div className="gradeIconPlaceholder"></div>
             </div>
-            <span className="gradeName">GOLD</span>
+            <span className="gradeName grade-gold">GOLD</span>
             <div className="gradeRequirements">
               <span className="gradeRequirement">우승 100 / 생성 100</span>
               <span className="gradeRequirement">이상 달성</span>
@@ -45,7 +45,7 @@ const Grade = () => {
             <div className="gradeIconWrapper profile-border-platinum">
               <div className="gradeIconPlaceholder"></div>
             </div>
-            <span className="gradeName">PLATINUM</span>
+            <span className="gradeName grade-platinum">PLATINUM</span>
             <div className="gradeRequirements">
               <span className="gradeRequirement">우승 500 / 생성 200</span>
               <span className="gradeRequirement">이상 달성</span>
@@ -55,7 +55,7 @@ const Grade = () => {
             <div className="gradeIconWrapper profile-border-diamond">
               <div className="gradeIconPlaceholder"></div>
             </div>
-            <span className="gradeName">DIAMOND</span>
+            <span className="gradeName grade-diamond">DIAMOND</span>
             <div className="gradeRequirements">
               <span className="gradeRequirement">우승 1000 / 생성 500</span>
               <span className="gradeRequirement">이상 달성</span>
@@ -65,7 +65,7 @@ const Grade = () => {
             <div className="gradeIconWrapper profile-border-master">
               <div className="gradeIconPlaceholder"></div>
             </div>
-            <span className="gradeName">MASTER</span>
+            <span className="gradeName grade-master">MASTER</span>
             <div className="gradeRequirements">
               <span className="gradeRequirement">월간 랭킹 1,2,3위</span>
               <span className="gradeRequirement">3회 이상 달성</span>
@@ -75,7 +75,7 @@ const Grade = () => {
             <div className="gradeIconWrapper profile-border-challenger">
               <div className="gradeIconPlaceholder"></div>
             </div>
-            <span className="gradeName challenger-shine">CHALLENGER</span>
+            <span className="gradeName challenger-shine grade-challenger">CHALLENGER</span>
             <div className="gradeRequirements">
               <span className="gradeRequirement">마스터 달성 후</span>
               <span className="gradeRequirement">랭킹 1위 3회 이상</span>
